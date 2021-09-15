3415e9f89de399e6fe0ec1198d6d77c035c01238
kevinchen2002 <kaifengchen2002@gmail.com>
2021-09-15 15:20:39 -0700

    TextUI - completed case 2
    UI supports adding new game

--- plainOldJava/src/main/java/cmpt276/as1/plainoldjava/text/TextUI.java
+++ plainOldJava/src/main/java/cmpt276/as1/plainoldjava/text/TextUI.java
@@ -32,7 +32,8 @@ public class TextUI {
                     int count = 0;
                     for (Game game : gameList) {
                         ArrayList<Integer> scores = game.getScores();
-                        System.out.println(count + ". ");
+                        int gameNum = count+1;
+                        System.out.println("\n\n" + gameNum + ". ");
                         for (int i = 0; i < scores.size(); i++) {
                             if (i != scores.size()-1) {
                                 System.out.print(scores.get(i) + " vs. ");
@@ -43,18 +44,60 @@ public class TextUI {
                         }
                         System.out.print(", winner player(s): " + game.getWinners());
                         System.out.print(", " + game.getTime());
+                        count++;
                     }
                     break;
 
                 case 2:
-                    System.out.println("case 2");
+
+                    int numPlayers = 0;
+                    while (numPlayers < 1 || numPlayers > 4) {
+                        System.out.println("How many players? (must be between 1 and 4)\n");
+                        numPlayers = in.nextInt();
+                    }
+
+                    Game game = new Game();
+
+                    for (int i = 0; i < numPlayers; i++) {
+                        int playerNum = i + 1;
+                        System.out.println("Player " + playerNum + ": ");
+                        System.out.println("    How many cards?");
+                        int numCards = in.nextInt();
+                        int cardPoints;
+                        int wagers;
+                        if (numCards == 0) {
+                            cardPoints = 0;
+                            wagers = 0;
+                        }
+                        else {
+                            System.out.println("    Sum of cards?");
+                            cardPoints = in.nextInt();
+                            System.out.println("    How many wagers?");
+                            wagers = in.nextInt();
+                        }
+                        game.playGame(i+1, numCards, cardPoints, wagers);
+                    }
+                    //print out the game
+                    ArrayList<Integer> scores = game.getScores();
+                    for (int i = 0; i < scores.size(); i++) {
+                        if (i != scores.size()-1) {
+                            System.out.print(scores.get(i) + " vs. ");
+                        }
+                        else {
+                            System.out.print(scores.get(i));
+                        }
+                    }
+                    System.out.print(", winner player(s): " + game.getWinners());
+                    System.out.print(", " + game.getTime());
+
+                    manager.add(game);
+                    break;
 
                 case 3:
                     System.out.println("case 3");
 
                 case 0:
                     isDone = true;
-                    System.out.println("case 0");
 
                 default:
                     System.out.println("Pick one of the above options!");
